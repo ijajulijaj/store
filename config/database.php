75cd68ccb698f4cf05d894c636860b00ba8113a5
IJA (Ijajul Islam Ijaj) <ija@agora-bd.com>
2025-11-23 15:28:49 +0600

    Database with ssl

--- config/database.php
+++ config/database.php
@@ -41,21 +41,21 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
             'host' => env('DB_HOST', 'appservicedb.mysql.database.azure.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'agorasup_groceries'),
             'username' => env('DB_USERNAME', 'appmysqladmin'),
             'password' => env('DB_PASSWORD', 'appmysqladmin#&%675'),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/home/site/wwwroot/DigiCertGlobalRootCA.crt.pem'),
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
             ]) : [],
         ],
