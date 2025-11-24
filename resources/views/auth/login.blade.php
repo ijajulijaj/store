e5a7193b2bdce59bec2da72865e04de24bb3e4ea
IJA (Ijajul Islam Ijaj) <ija@agora-bd.com>
2025-11-24 16:47:38 +0600

    Modify

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
+
+  <!-- Browser tab-->
   <title>Agora Superstores - Admin Panel</title>
   
   <!-- Favicon -->
