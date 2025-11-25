c0ace15314e5930941f408c5a7762729db671f85
IJA (Ijajul Islam Ijaj) <ija@agora-bd.com>
2025-11-25 12:05:09 +0600

    Delete middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(\App\Http\Middleware\ForceHttps::class);
+
+        $middleware->trustProxies(at: '*')->trustForwardedHeaders();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })
-    ->create();
-
+    })->create();
