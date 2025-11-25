9ac4c299a8df1e4b492d073c65c1a498d89b1b39
IJA (Ijajul Islam Ijaj) <ija@agora-bd.com>
2025-11-25 10:47:54 +0600

    Design changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,15 +11,15 @@
   <title>Agora Superstores - Admin Panel</title>
   
   <!-- Favicon -->
-  <link rel="icon" href="{{ asset('public/admin_assets/img/favicon.png') }}" type="image/png">
+  <link rel="icon" href="{{ asset('admin_assets/img/favicon.png') }}" type="image/png">
   
   <!-- Custom fonts for this template-->
-  <link href="{{ asset('public/admin_assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('admin_assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   
   <!-- Custom styles for this template-->
-  <link href="{{ asset('public/admin_assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
-  <link href="{{ asset('public/admin_assets/css/style.css') }}" rel="stylesheet">
+  <link href="{{ asset('admin_assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('admin_assets/css/style.css') }}" rel="stylesheet">
 </head>
 <body class="bg-gradient-primary">
   <div class="container d-flex align-items-center justify-content-center min-vh-100">
@@ -31,7 +31,7 @@
             <!-- Nested Row within Card Body -->
             <div class="row">
               <div class="col-lg-6 d-none d-lg-flex">
-                <img src="{{ asset('public/admin_assets/img/logo.png') }}" alt="profile Pic" class="custom-image-size left-center">
+                <img src="{{ asset('admin_assets/img/logo.png') }}" alt="profile Pic" class="custom-image-size left-center">
               </div>
               <div class="col-lg-6">
                 <div class="p-5">
@@ -75,11 +75,11 @@
     </div>
   </div>
   <!-- Bootstrap core JavaScript-->
-  <script src="{{ asset('public/admin_assets/vendor/jquery/jquery.min.js') }}"></script>
-  <script src="{{ asset('public/admin_assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('admin_assets/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('admin_assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <!-- Core plugin JavaScript-->
-  <script src="{{ asset('public/admin_assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+  <script src="{{ asset('admin_assets/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
   <!-- Custom scripts for all pages-->
-  <script src="{{ asset('public/admin_assets/js/sb-admin-2.min.js') }}"></script>
+  <script src="{{ asset('admin_assets/js/sb-admin-2.min.js') }}"></script>
 </body>
 </html>
