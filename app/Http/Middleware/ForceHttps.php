dcc9e4d042b302f19441d11cc54a7f6937fe22cb
IJA (Ijajul Islam Ijaj) <ija@agora-bd.com>
2025-11-25 11:50:10 +0600

    Created middleware

---
+++ app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ForceHttps
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Force all generated URLs to use HTTPS
+        $request->setTrustedProxies([$request->getClientIp()], Request::HEADER_X_FORWARDED_ALL);
+
+        if (!$request->secure()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
